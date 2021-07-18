b6215a7d79700a7e89aaba52b69424fc476836cf
Jackie <JChan106@ucr.edu>
2021-07-18 15:23:01 -0700

    add experience and new resume

--- src/components/data.js
+++ src/components/data.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React from "react"
 
 export default [
   {
-    title: 'About',
+    title: "About",
     detail: (
       <>
         <div>About Me</div>
-          <ul>
-            <li>Hi! I am a full stack software engineer with a passion for building web applications. I also enjoy playing basketball, gaming, and fashion. Here is a quote
-            that I follow from one of my favorite actors:<br/>
-            <i>"Don't try to be like Jackie. There is only one Jackie. Study computers instead."<br/>-Jackie Chan, Actor</i>
-            </li>
-          </ul>
+        <ul>
+          <li>
+            Hi! I am a full-stack software engineer with a passion for building
+            web applications. I also enjoy playing basketball, gaming, and
+            fashion. Here is a quote that I follow from one of my favorite
+            actors:
+            <br />
+            <i>
+              "Don't try to be like Jackie. There is only one Jackie. Study
+              computers instead."
+              <br />
+              -Jackie Chan, Actor
+            </i>
+          </li>
+        </ul>
         <div>Education</div>
         <ul>
           <li> University of California, Riverside </li>
-           <ul>
+          <ul>
             <li>B.S. Computer Science</li>
             <li>Graduated June 2018</li>
           </ul>
@@ -25,7 +34,7 @@ export default [
     key: 1,
   },
   {
-    title:'Skills',
+    title: "Skills",
     detail: (
       <>
         <div>Languages</div>
@@ -34,7 +43,6 @@ export default [
           <li>C#</li>
           <li>C++</li>
           <li>Terraform</li>
-          <li>Bash</li>
           <li>SQL</li>
           <li>Excel VBA</li>
         </ul>
@@ -67,55 +75,133 @@ export default [
     key: 2,
   },
   {
-    title:'Projects',
+    title: "Projects",
     detail: (
       <>
-        <div><img src='https://i.imgur.com/k3OP6Kf.gif' alt="DoodlyDoo" width="80" height="125"/>DoodlyDoo</div>
+        <div>
+          <img
+            src="https://i.imgur.com/k3OP6Kf.gif"
+            alt="DoodlyDoo"
+            width="80"
+            height="125"
+          />
+          DoodlyDoo
+        </div>
         <ul>
-          <li>Created an appointment app, which allows users to plan events.</li>
-          <li>Main features include a friends list, user permissions of who they can invite, commenting section of appointments, and more.</li>
+          <li>
+            Created an appointment app, which allows users to plan events.
+          </li>
+          <li>
+            Main features include a friends list, user permissions of who they
+            can invite, commenting section of appointments, and more.
+          </li>
           <li>Built using React Native, Redux, Firebase, and Bootstrap.</li>
         </ul>
-        <div><img src='https://i.imgur.com/rMnXxkj.gif' alt="PostPin" width="125" height="125"/>PostPin</div>
+        <div>
+          <img
+            src="https://i.imgur.com/rMnXxkj.gif"
+            alt="PostPin"
+            width="125"
+            height="125"
+          />
+          PostPin
+        </div>
         <ul>
-          <li>Created a Multi-page web app which provides College students a platform to Buy/Sell/Trade</li>
-          <li> Made using good practices: collaborators made pull requests, Kanban board to organize tasks, weekly meetings</li>
+          <li>
+            Created a Multi-page web app which provides College students a
+            platform to Buy/Sell/Trade
+          </li>
+          <li>
+            Made using good practices: collaborators made pull requests, Kanban
+            board to organize tasks, weekly meetings
+          </li>
           <li>Built using: ReactJS, Sass, Bootstrap, Firebase</li>
         </ul>
-        <div className='title'>LED Game</div>
+        <div className="title">LED Game</div>
         <ul>
           <li>Created an asteroid dodging game on a 32x32 LED Board</li>
           <li>Used USART communication between the Arduino and Atmega</li>
-          <li>Built using: Arduino, Atmega1284, LCD Screen for the score, 32x32 LED Board for display, and joystick for movement</li>
+          <li>
+            Built using: Arduino, Atmega1284, LCD Screen for the score, 32x32
+            LED Board for display, and joystick for movement
+          </li>
         </ul>
-        <div className='title'>Tanks</div>
+        <div className="title">Tanks</div>
         <ul>
-          <li>Created a 2 player real-time tank game using Unity and EA Pathfinders API</li>
+          <li>
+            Created a 2 player real-time tank game using Unity and EA
+            Pathfinders API
+          </li>
           <li>Made during Hackpoly 2016, won the EA Pathfinders prize</li>
-          <li>The EA Pathfinders API allowed us to use a smartphone as a controller for the game</li>
+          <li>
+            The EA Pathfinders API allowed us to use a smartphone as a
+            controller for the game
+          </li>
         </ul>
       </>
     ),
     key: 3,
   },
   {
-    title:'Experience',
+    title: "Experience",
     detail: (
       <>
+        <div>AptEdge - Senior Software Engineer</div>
+        <ul>
+          <li>
+            Constructed and developed the core routing, listing pages, and
+            detail pages for our workflow product which decreased support agents
+            time spent searching and closing tickets.
+          </li>
+          <li>
+            Built custom tooltip, multi-select, input, dropdown components using
+            React Context.
+          </li>
+          <li>
+            Worked directly with customers to create workflow features which
+            included File upload using cloud storage, user commenting, CRUD
+            actions, and merging entities.
+          </li>
+          <li>
+            Improved user experience by establishing consistent loading and
+            error handling toasts and messages throughout our app.
+          </li>
+          <li>
+            Added and managed Google Analytics for our marketing website, web
+            app, and support system apps to measure customer engagement of
+            certain features.
+          </li>
+          <li>
+            Developed functionality for customers to save sets of filters across
+            multiple types of data sets.
+          </li>
+        </ul>
         <div>Kelley Blue Book - Full Stack Software Engineer</div>
-          <ul>
-            <li>Worked in a scrum team which developed consumer facing applications for the automotive industry.</li>
-            <li>Created user interfaces, APIs, and Terraform to build the infrastructure of our applications.</li>
-            <li>Decomposed a monolith into micro-services while adding additional functionality to each of the applications.</li>
-          </ul>
+        <ul>
+          <li>
+            Worked in a scrum team which developed consumer facing applications
+            for the automotive industry.
+          </li>
+          <li>
+            Created user interfaces, APIs, and Terraform to build the
+            infrastructure of our applications.
+          </li>
+          <li>
+            Decomposed a monolith into micro-services while adding additional
+            functionality to each of the applications.
+          </li>
+        </ul>
         <div>Center for Environmental Research and Technology</div>
-          <ul>
-            <li>Managed the solar energy website which uses PHP and SQL</li>
-            <li>Reduced workload for researchers by creating Excel VBA Macros</li>
-            <li>Excel VBA Macros transferred and analyzed energy data and made multiple graphs and calculations from it</li>
-          </ul>
+        <ul>
+          <li>Managed the solar energy website which uses PHP and SQL</li>
+          <li>Reduced workload for researchers by creating Excel VBA Macros</li>
+          <li>
+            Excel VBA Macros transferred and analyzed energy data and made
+            multiple graphs and calculations from it
+          </li>
+        </ul>
       </>
     ),
     key: 4,
-  }
+  },
 ]
